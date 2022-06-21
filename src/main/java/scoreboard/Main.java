80624a5318ac36b8efc0791132b70c5391fa7d81
juanBautista <juanbautista19@gmail.com>
2022-06-21 22:36:06 +0200

    Adding a call for startGame method in Main java class

--- src/main/java/scoreboard/Main.java
+++ src/main/java/scoreboard/Main.java
@@ -1,9 +1,24 @@
 package scoreboard;
 
+import scoreboard.domain.AwayTeam;
+import scoreboard.domain.Game;
+import scoreboard.domain.HomeTeam;
+import scoreboard.impl.ScoreBoardImpl;
+
 public class Main {
+
+  private static final ScoreBoard scoreBoard = new ScoreBoardImpl();
 
   public static void main(String[] args) {
 
+    Game gameUruguayItaly = Game.builder()
+      .homeTeam(HomeTeam.builder().name("Uruguay").build())
+      .awayTeam(AwayTeam.builder().name("Italy").build())
+      .build();
+
+    // Starting first game
+    gameUruguayItaly = scoreBoard.startGame(gameUruguayItaly);
+    System.out.println(gameUruguayItaly.toString());
   }
 
 }
